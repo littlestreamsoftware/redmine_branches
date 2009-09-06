dd1f586055f3ae52ee09422f7de3c41885181fad
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-06 12:59:07 -0700

    Fixed some DEPRECATION warnings in the tests

--- test/functional/queries_controller_test.rb
+++ test/functional/queries_controller_test.rb
@@ -70,7 +70,7 @@ class QueriesControllerTest < ActionController::TestCase
          :query => {"name" => "test_new_project_public_query", "is_public" => "1"}
          
     q = Query.find_by_name('test_new_project_public_query')
-    assert_redirected_to :controller => 'issues', :action => 'index', :query_id => q
+    assert_redirected_to :controller => 'issues', :action => 'index', :project_id => 'ecookbook', :query_id => q
     assert q.is_public?
     assert q.has_default_columns?
     assert q.valid?
@@ -88,7 +88,7 @@ class QueriesControllerTest < ActionController::TestCase
          :query => {"name" => "test_new_project_private_query", "is_public" => "1"}
          
     q = Query.find_by_name('test_new_project_private_query')
-    assert_redirected_to :controller => 'issues', :action => 'index', :query_id => q
+    assert_redirected_to :controller => 'issues', :action => 'index', :project_id => 'ecookbook', :query_id => q
     assert !q.is_public?
     assert q.has_default_columns?
     assert q.valid?
@@ -104,7 +104,7 @@ class QueriesControllerTest < ActionController::TestCase
          :query => {"name" => "test_new_global_private_query", "is_public" => "1", "column_names" => ["", "tracker", "subject", "priority", "category"]}
          
     q = Query.find_by_name('test_new_global_private_query')
-    assert_redirected_to :controller => 'issues', :action => 'index', :query_id => q
+    assert_redirected_to :controller => 'issues', :action => 'index', :project_id => nil, :query_id => q
     assert !q.is_public?
     assert !q.has_default_columns?
     assert_equal [:tracker, :subject, :priority, :category], q.columns.collect {|c| c.name}
