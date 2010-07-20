ab168518e1cde6b240173a41e829e94a6db2f3b7
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-20 14:46:14 -0700

    Lower caching size to 2 bytes.

--- lib/redmine/wiki_formatting.rb
+++ lib/redmine/wiki_formatting.rb
@@ -44,7 +44,7 @@ module Redmine
       end
       
       def to_html(format, text, options = {}, &block)
-        text = if Setting.cache_formatted_text? && text.size > 2.kilobyte && cache_store && cache_key = cache_key_for(format, options[:object], options[:attribute])
+        text = if Setting.cache_formatted_text? && text.size > 2.byte && cache_store && cache_key = cache_key_for(format, options[:object], options[:attribute])
           # Text retrieved from the cache store may be frozen
           # We need to dup it so we can do in-place substitutions with gsub!
           cache_store.fetch cache_key do
